ee39e5214b69e55a8eb17813cf4627c756f81132
Dvir Cohen <dvirco123@gmail.com>
2023-06-14 13:52:55 +0300

    Svg service script - done

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const fs = require('fs');
 const path = require('path')
 
-const svgFolderPath = './src/assets/svg/';
-const outputFile = './src/services/svgData.js';
+const svgFolderPath = './svgs';
+const outputFile = './output/svg-service.js';
 
 const svgFiles = fs.readdirSync(svgFolderPath);
 console.log('svgFiles: ', svgFiles);
+
+const svgData = {};
+svgFiles.forEach((fileName) => {
+    if (path.extname(fileName).toLowerCase() === '.svg') {
+        const svgContent = fs.readFileSync(path.join(svgFolderPath, fileName), 'utf8');
+        const svgName = path.basename(fileName, '.svg');
+        console.log('svgName: ', svgName);
+        svgData[svgName] = svgContent;
+    }
+  });
+//   console.log('svgData: ', svgData);
+
+// Only the SVGs object
+// const fileContent = `const svgs = ${JSON.stringify(svgData, null, 2)}\n\nexport default svgs\n`
+
+// With get SVG function
+const fileContent = `const svgs = ${JSON.stringify(svgData, null, 2)}\n\nfunction getSvg(iconName) {\n  return svgs[iconName]\n}\n\nexport const svgService = {\n  getSvg\n}\n`
+  
+fs.writeFileSync(outputFile, fileContent);
 
 
 function generateSvgData() {
